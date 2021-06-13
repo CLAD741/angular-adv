b571ddce77efc506391f7f231d2df0ec1aac8ac0
CristianUniandes <cl.arango741@uniandes.edu.co>
2021-06-12 20:33:44 -0500

    Fin sección routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './appRouting.module';
+import { PagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule, 
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
